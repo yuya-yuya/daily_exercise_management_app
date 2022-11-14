4d41f54cdd46fbdddd47a5c3bcaa6409a829ebf3
yuya-yuya <yytyfp5115@icloud.com>
2022-11-14 19:07:41 +0900

    userのroute定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: :registrations, controllers: {
+    sessions: 'sessions',
+    passwords: 'passwords',
+    confirmations: 'confirmations',
+  }
+
+  devise_scope :users do
+    # devise_forで定義すると不要なedit/destroyまで生成されてしまうので個別に定義する
+    resource :registration, only: [:new, :create]
+  end
+
   root to: 'homes#top'
 end
